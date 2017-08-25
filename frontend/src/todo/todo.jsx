5420a301b221f77a5d0d9d213f33e7ea58df7f5d
dsamuel32 <diego.samuel.alves@gmail.com>
2017-08-25 20:17:49 -0300

    Add evento add.

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,6 +5,11 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
     
     handleAdd() {
         alert('funciona')
