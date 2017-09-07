b157c69ca48ba0f43a02e7f5266c83221d666ad0
dsamuel32 <diego.samuel.alves@gmail.com>
2017-09-07 14:10:18 -0300

    fRemovendo imports sem utilização.

--- frontend-redux/src/todo/todo.jsx
+++ frontend-redux/src/todo/todo.jsx
@@ -1,12 +1,8 @@
 import React, {Component} from 'react'
 
-import axios from 'axios'
-
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
-
-const URL = 'http://localhost:8080/api/todos'
 
 export default class Todo extends Component {
 
